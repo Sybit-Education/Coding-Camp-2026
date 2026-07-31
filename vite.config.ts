0446e952c9239686454eb51faa7334d599dce0a6
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2026-07-31 11:26:30 +0200

    feat: autolint using osx

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Oxlint from 'unplugin-oxlint/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Oxlint({
+      path: 'src', // Scannt deinen src-Ordner
+      watch: true, // Linter läuft live im Hintergrund mit
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    host: true, // Wichtig für den Devcontainer!
+    port: 5173
+  }
 })
