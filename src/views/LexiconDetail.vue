68bc7ba3ef845aa18fd98b34476f3b6cfd981fbc
GolddHallo <pino.hirsch10@gmx.de>
2026-08-04 11:41:19 +0000

    feat: added LexiconDetail

---
+++ src/views/LexiconDetail.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <img src="../assets/logo.svg" alt="" class="rounded-3xl p-4">
+<h1 class="text-3xl font-bold text-center">{{title}}</h1>
+<p class="p-4">{{description}}</p>
+</div>
+
+</template>
+<script setup lang="ts">
+const title = 'Haubentaucher'
+const description = 'Der Haubentaucher ist eine Vogelart aus der Familie der Lappentaucher.'
+</script>
