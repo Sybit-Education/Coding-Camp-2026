83b92c801206398e3ae5565f5653bc477845fc7e
Bergstaedt, Jimmy <Jimmy.Bergstaedt@sybit.de>
2026-08-04 13:56:00 +0200

    feat: ImpressumView.vue

--- src/router/route-names.ts
+++ src/router/route-names.ts
@@ -1,3 +1,4 @@
 export const ROUTE_NAMES = {
   HOME: 'home',
+  IMPRESSUM: 'impressum',
 } as const
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,17 @@
 import HomeView from '@/views/HomeView.vue'
 import type { RouteRecordRaw } from 'vue-router'
 import { ROUTE_NAMES } from './route-names'
+import ImpressumView from '@/views/ImpressumView.vue'
 
 export const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: ROUTE_NAMES.HOME,
     component: HomeView,
+  },
+  {
+    path: '/impressum',
+    name: ROUTE_NAMES.IMPRESSUM,
+    component: ImpressumView,
   },
 ]
