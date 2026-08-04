607d2e43f38071602871287221962cdd318cb658
Hasenflopp <exsamkap@gmail.com>
2026-08-04 10:40:43 +0000

    feat: added colors and base button style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <h1 class="text-3xl font-bold underline text-primary">Hello world!</h1>
+  <h1 class="text-3xl font-bold underline text-secondary">Hello world2!</h1>
+
+  <button class="btn btn-primary">Zur Karte</button>
+  <button class="btn btn-secondary">Infos</button>
 </template>
