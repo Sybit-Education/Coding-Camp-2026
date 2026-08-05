7032c3c6db141a67dd9cc54c439b738e201b0522
Frank, Elias <Elias.Frank@sybit.de>
2026-08-05 09:13:43 +0200

    fix: removed not existing service from service-registery

--- src/bootstrap/service-registry.ts
+++ src/bootstrap/service-registry.ts
@@ -1,5 +1,4 @@
 import { MapService } from '@/services/map.service'
-import { BirdRecognitionService } from '@/services/bird-recognition.service'
 import type { App } from 'vue'
 import type { Router } from 'vue-router'
 import { PocketBaseService } from '@/services/pocket-base.service'
@@ -7,14 +6,12 @@ import { LexiconService } from '@/services/lexicon.service'
 
 export function createAppServices(_router: Router) {
   const mapService = new MapService()
-  const birdRecognitionService = new BirdRecognitionService()
   const pocketBaseService = new PocketBaseService()
   const lexiconService = new LexiconService(pocketBaseService)
 
   return {
     pocketBaseService,
     mapService,
-    birdRecognitionService,
     lexiconService,
   }
 }
@@ -24,6 +21,5 @@ export type AppServices = ReturnType<typeof createAppServices>
 export function provideAppServices(app: App, services: AppServices): void {
   app.provide('pocketBaseService', services.pocketBaseService)
   app.provide('mapService', services.mapService)
-  app.provide('birdRecognitionService', services.birdRecognitionService)
   app.provide('lexiconService', services.lexiconService)
 }
