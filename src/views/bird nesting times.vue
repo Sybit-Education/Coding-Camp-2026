c2f80aa48542372859935e34b5243b42759e7dc8
Patrick1234KLopfer <lars.klopfer.09@gmail.com>
2026-08-06 08:48:10 +0000

    feat: Nistzeiten von allen Vögeln

---
+++ src/views/bird nesting times.vue
@@ -0,0 +1,15 @@
+<template>
+  Die Flussseeschwalbe nistet und brütet in der Zeit von Mai bis Juni Kormorane nisten und brüten im
+  Frühjahr und Sommer.Die Hauptbrut- und Nistzeit des Graureihers erstreckt sich von März bis Juni
+  Die Nist- und Brutzeit der Nachtigall in Mitteleuropa findet hauptsächlich in den Monaten Mai und
+  Juni Die Nist- und Brutzeit der Gartengrasmücke findet hauptsächlich von Mai bis Juli Die
+  Hauptbrut- und Nistzeit der Mönchsgrasmücke liegt zwischen April und Juli (gelegentlich bis
+  August). Der Drosselrohrsänger nistet in der Zeit von Mai bis August. die Hauptbrutzeit und
+  Nistzeit des Teichrohrsängers erstreckt sich von Mai bis Juli Der Baumfalke nistet und brütet in
+  der Zeit von Anfang Juni bis August/September. Der Grünschenkel nistet in der Zeit von Mai bis
+  Juli. Eichelhäher nisten von April bis Juni Der Grünspecht nistet im Frühjahr von April bis Juni.
+  Die Nist- und Brutzeit des Buntspechts beginnt im April und dauert bis Juni oder Juli. Die Nist-
+  und Brutzeit des Wiedehopfs findet im Mein schöner Garten von April bis Juli statt. Die Hauptbrut-
+  und Nistzeit des Pirols liegt in den Monaten Mai und Juni Die Hauptnist- und Brutzeit des
+  Mittelspechts erstreckt sich in Mitteleuropa von März/April bis Juli,
+</template>
