8b855a8378f69a31aaf755cbd12bab3d2b3300bf
yblacky <schwarz.yannik@online.de>
2026-08-07 10:49:46 +0200

    style: move App.vue script block below the template

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,3 @@
-<script setup lang="ts">
-import Navbar from '@/components/Navbar.vue'
-import HeaderComponent from '@/components/HeaderComponent.vue'
-import { RouterView, useRoute } from 'vue-router'
-import { computed } from 'vue'
-
-const route = useRoute()
-const showHeader = computed(() => route.name !== 'map')
-</script>
-
 <template>
   <div class="app-shell">
     <HeaderComponent v-if="showHeader" class="shrink-0" />
@@ -19,3 +9,13 @@ const showHeader = computed(() => route.name !== 'map')
     <Navbar />
   </div>
 </template>
+
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import { RouterView, useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+const showHeader = computed(() => route.name !== 'map')
+</script>
