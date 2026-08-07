92c84c7428509209a120c976574d4964f1f852d9
huetteljannick <jannick@huettel.dev>
2026-08-07 08:48:57 +0200

    fix: loading from DB

--- src/components/Warning.vue
+++ src/components/Warning.vue
@@ -1,59 +1,26 @@
 <template>
   <main class="mx-auto w-full max-w-md space-y-4 px-4 py-4 pb-24">
-    <section class="space-y-4" aria-label="Lexikoneinträge">
-      <Warning v-for="entry in filteredEntries" :key="entry.id" :entry="entry" />
+    <section class="space-y-4" aria-label="Warnungen">
+      <div v-for="entry in entries" :key="entry.id" :entry="entry">
+        <p>{{ entry.heading }}</p>
+      </div>
     </section>
   </main>
 </template>
 
 <script setup lang="ts">
-//import Warning from '@/components/Warning.vue'
 import { WarningService } from '@/services/warning.service'
 import type { Warning } from '@/shared/types/warning.types'
-import { computed, inject, onBeforeUnmount, onMounted, ref } from 'vue'
+import { inject, onMounted, ref } from 'vue'
 
 defineOptions({
   name: 'HomeView',
 })
 
-const warningService = inject('warningService') as WarningService
-
+const warningService = inject<WarningService>('warningService')!
 const entries = ref<Warning[]>([])
-const isMenuOpen = ref(false)
-const selectedLabel = ref('')
-const menuRef = ref<HTMLElement | null>(null)
-const searchQuery = ref('')
-
-const labels = computed(() =>
-  labelsStore.getLabels
-    .map((label) => label.name)
-    .sort((first, second) => first.localeCompare(second)),
-)
-
-const filteredEntries = computed(() => {
-  let result = lexiconService.filterLexiconEntries(entries.value, searchQuery.value)
-
-  if (selectedLabel.value) {
-    result = result.filter((entry) => entry.label === selectedLabel.value)
-  }
-
-  return result
-})
-
-const toxicityReferences = computed(() => toxicityStore.getAllToxicityLevels)
-
-function closeMenuOnOutsideClick(event: PointerEvent) {
-  if (menuRef.value && event.target instanceof Node && !menuRef.value.contains(event.target)) {
-    isMenuOpen.value = false
-  }
-}
 
 onMounted(async () => {
-  document.addEventListener('pointerdown', closeMenuOnOutsideClick)
-  entries.value = await lexiconService.getLexiconEntriesList()
-})
-
-onBeforeUnmount(() => {
-  document.removeEventListener('pointerdown', closeMenuOnOutsideClick)
+  entries.value = await warningService.getWarnings()
 })
 </script>
--- src/services/warning.service.ts
+++ src/services/warning.service.ts
@@ -3,16 +3,6 @@ import type { PocketBaseService } from './pocket-base.service'
 
 export class WarningService {
   constructor(readonly pocketBaseService: PocketBaseService) {}
-
-  async getWarningById(id: string): Promise<Warning> {
-    const warning = await this.pocketBaseService.getById<Warning>('warnings', id)
-    const result: Warning = {
-      ...warning,
-    }
-
-    return result
-  }
-
   async getWarnings(): Promise<Warning[]> {
     return await this.pocketBaseService.getAll('warnings')
   }
